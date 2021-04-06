40cedce9ca6a976af7bafe24a1faf59d24077f77
Michael Richters <gedankenexperimenter@gmail.com>
2021-04-06 18:55:59 -0500

    Adapt TypingBreaks plugin to KeyEvent handlers
    Signed-off-by: Michael Richters <gedankenexperimenter@gmail.com>

--- plugins/Kaleidoscope-TypingBreaks/src/kaleidoscope/plugin/TypingBreaks.h
+++ plugins/Kaleidoscope-TypingBreaks/src/kaleidoscope/plugin/TypingBreaks.h
@@ -36,7 +36,7 @@ class TypingBreaks : public kaleidoscope::Plugin {
 
   static settings_t settings;
 
-  EventHandlerResult onKeyswitchEvent(Key &mapped_key, KeyAddr key_addr, uint8_t key_state);
+  EventHandlerResult onKeyEvent(KeyEvent &event);
   EventHandlerResult onFocusEvent(const char *command);
   EventHandlerResult onSetup();
 
